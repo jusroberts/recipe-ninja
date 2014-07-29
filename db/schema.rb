8f6b433efe5465d7807ad3bd97091a05cc7ce500
Justin Roberts <justin.roberts@careerbuilder.com>
2014-07-29 08:34:21 -0400

    stuff

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140727202921) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "recipes", force: true do |t|
     t.string   "url"
     t.integer  "shopping_list_id"
